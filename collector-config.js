1383a1b53e303f59b80bb6d0d8ab2b53f60fbfa9
Maria202000 <g2432042@tcu.ac.jp>
2026-09-14 17:43:57 +0900

    Update collector-config.js

--- collector-config.js
+++ collector-config.js
@@ -1,4 +1,5 @@
 // Paste the Google Apps Script web app URL here.
-window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbz8WszqHoSUvNBcHpbPh_QzOuMjk15W5axcTRF3XVJVisr_7Y1hVjktmk7BKV5pFvBFnw/exec";
+window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbxcc5CRyBguee3bFiyTRKvIILembxOKWOaRan3BFNt47usSbL9WsvTuQvWO4vhhc_X50g/exec";
+
 
 
