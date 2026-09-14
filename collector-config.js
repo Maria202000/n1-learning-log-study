a5079e97a3f1745dfd33504845c1025eb477a118
Maria202000 <g2432042@tcu.ac.jp>
2026-09-14 18:14:08 +0900

    Add files via upload

--- collector-config.js
+++ collector-config.js
@@ -1,3 +1,2 @@
 // Paste the Google Apps Script web app URL here.
 window.LEARNING_LOG_COLLECTOR_URL = "https://script.google.com/macros/s/AKfycbxcc5CRyBguee3bFiyTRKvIILembxOKWOaRan3BFNt47usSbL9WsvTuQvWO4vhhc_X50g/exec";
-
